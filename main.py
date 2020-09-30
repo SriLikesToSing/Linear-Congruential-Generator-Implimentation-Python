904db54805dfc810e7fd924400409faa6d5780bd
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2020-09-30 12:22:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,24 @@ def generate(m, a, c, X0, length):
 
     return list
 
+'''Algorithm description
 
+    Xn+1 = (a*Xn + c ) mod m
+'''
 
+'''
+    m, m > 0 --- "modulus"
+    a, 0 < a < m  ---- the "multiplier"
+    c, 0<= c < m --- the "increment"
+    X0, 0 <= X0 < m --- the "seed" or "start value"
+'''
 
-
+#numerical recipies
 list = generate(2**32, 1664525, 1013904223, 1,  5)
+
+#Borland C/C++
+list = generate(2**32, 22695477, 1, 1,  5)
+
 
 for x in list:
     print(x)
